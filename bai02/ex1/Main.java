dec7ad817b950728e4d49693ec3a5d74df2ccd3c
p3dm <manh.pd225361@sis.hust.edu.vn>
2024-03-14 14:44:37 +0700

    Add sayHello method

--- bai02/ex1/Main.java
+++ bai02/ex1/Main.java
@@ -1,10 +1,13 @@
 package bai02.ex1;
-import java.util.Scanner;
 
+import java.util.Scanner;
 /*
-Viết khai báo một gói chứa hai lớp: lớp hình vuông, lớp hình tròn. Viết khai báo lớp hình vuông, lớp hình tròn cùng
-các thuộc tính thích hợp, các phương thức get/set thích hợp.
-*/
+• Bài 1: Viết khai báo một gói (shape) chứa hai lớp:
+lớp hình vuông, lớp hình tròn. Viết khai báo lớp hình vuông, lớp hình tròn cùng các thuộc tính thích hợp,
+các phương thức get/set thích hợp.
+• Thuộc tính: Kích thước (m)
+• Phương thức: get/set và sayhello
+• Viết chương trình sử dụng 2 lớp này*/
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -13,13 +16,16 @@ public class Main {
         double squareSide = scanner.nextDouble();
         Square square = new Square(0); // Khởi tạo với giá trị mặc định
         square.setSide(squareSide);
+        Square.sayHello();
         System.out.println("Diện tích hình vuông: " + square.getArea());
         System.out.println("Chu vi hình vuông: " + square.getPerimeter());
 
         System.out.print("Nhập bán kính hình tròn: ");
         double circleRadius = scanner.nextDouble();
         Circle circle = new Circle(0); // Khởi tạo với giá trị mặc định
+
         circle.setRadius(circleRadius);
+        Circle.sayHello();
         System.out.println("Diện tích hình tròn: " + circle.getArea());
         System.out.println("Chu vi hình tròn: " + circle.getPerimeter());
 
@@ -44,6 +50,9 @@ public class Main {
         public void setSide(double side) {
             this.side = side;
         }
+        public static void sayHello() {
+            System.out.println("Hình vuông");
+        }
     }
 
     static class Circle {
@@ -63,6 +72,9 @@ public class Main {
 
         public void setRadius(double radius) {
             this.radius = radius;
+        }
+        public static void sayHello() {
+            System.out.println("Hình Tròn");
         }
     }
 }
