8cfa690b6da5167d8fbdb64208276b9269f12af7
p3dxm <ripzkid2k4lore@gmail.com>
2024-04-22 22:23:44 +0700

    Ra dc co ban roi nhung ma nhieu cai thi chua biet lam

--- bai05/ex06/Test.java
+++ bai05/ex06/Test.java
@@ -4,6 +4,17 @@ package bai05.ex06;
 
 public class Test {
     public static void main(String[] args) {
-        String touristName = Random.touristName();
+        Tourist tourist = new Tourist("John");
+        Location location = new Location("Hanoi");
+        Hotel hotel = new Hotel("Hilton");
+        Tour tour = new Tour(tourist, location, hotel);
+        tour.book();
+        System.out.println(tour.getTourist().getTouristName());
+        System.out.println(tour.getLocation().getLocation());
+        System.out.println(tour.getHotel().getHotelName());
+        TourManagement tourManagement = new TourManagement();
+        tourManagement.bookTour(tourist, location, hotel);
+        System.out.println(tourManagement.getTotalBookedTours());
+    }
 
 }
